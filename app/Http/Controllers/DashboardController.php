be99dab49c7b83e8ef22f8c04f213d8d1708cfdf
fajarmagsyar <fajar.magsyar@gmail.com>
2024-03-25 09:47:33 +0700

    minor fix

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,9 +8,20 @@ class DashboardController extends Controller
 {
     public function index()
     {
+        $breadcrumb = [
+            [
+                'link' => '/',
+                'nama' => 'SIPO'
+            ],
+            [
+                'link' => '',
+                'nama' => 'Data Obat'
+            ]
+        ];
         $data = [
             'title' => 'Dashboard',
             'link' => 'dashboard',
+            'breadcrumb' => $breadcrumb
         ];
         return view('dashboard.index', $data);
     }
